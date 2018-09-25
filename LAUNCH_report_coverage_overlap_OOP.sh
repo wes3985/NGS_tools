2af055e505d67e973ac8ccc8da3f6b773d0f5d0b
Wes Woollard <wes.woollard3985@gmail.com>
2018-09-25 09:36:49 +0100

    removed typo from LAUNCH_report_coverage_overlap_OOP.py

--- LAUNCH_report_coverage_overlap_OOP.sh
+++ LAUNCH_report_coverage_overlap_OOP.sh
@@ -9,8 +9,7 @@ module load compilers/gnu/4.9.2
 module load python3/recommended
 
 # EXAMPLE OF HOW TO CALL THIS SCRIPT:
-# qsub $HOME/Scratch/git_single_cell_sequencing_scripts/LAUNCH_report_coverage_overlap_OOP.sh $HOME/Scratch/merged_bam_analysis/coverage_test_file_100k.txt "2" "AE,BE,HE,C" 
-$HOME/Scratch/merged_bam_analysis/coverage_test_file_100k_OUTPUT.txt
+# qsub $HOME/Scratch/git_single_cell_sequencing_scripts/LAUNCH_report_coverage_overlap_OOP.sh $HOME/Scratch/merged_bam_analysis/coverage_test_file_100k.txt "2" "AE,BE,HE,C" $HOME/Scratch/merged_bam_analysis/coverage_test_file_100k_OUTPUT.txt
 
 infile=$1
 minCov=$2
